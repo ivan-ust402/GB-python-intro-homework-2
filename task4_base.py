ef4ba7eb1bf7c74a1e3f24fdcfb18cf3e8cdc7dd
Ivan Ustyantsev <vangold915@gmail.com>
2022-11-29 19:36:13 +0700

    Completed task 4

--- task4_base.py
+++ task4_base.py
@@ -3,3 +3,13 @@
 Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
 Если в слово длинное, выводить только первые 10 букв в слове.
 """
+def print_formatting_message(string):
+    """ Message Formatting Method """
+    intermediate_list = string.split(' ')
+    for i, el_list in enumerate(intermediate_list, 1):
+        if len(el_list) > 10:
+            el_list = el_list[0:10]
+        print(f'Строка {i}: {el_list}')
+
+input_string = input("Введите ваше сообщение: ")
+print_formatting_message(input_string)
