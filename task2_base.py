e4f72e3465869773ff7c74d1d2af1f5efeba95e9
Ivan Ustyantsev <vangold915@gmail.com>
2022-11-29 18:27:01 +0700

    Created extra function replace_pair_another

--- task2_base.py
+++ task2_base.py
@@ -7,28 +7,41 @@
 def create_list(array_size):
     """ Creating and Filling list """
     array = []
-    i = 0
-    for _ in range(array_size):
+    for i in range(array_size):
         array.append(input(f'Введите {i} строчный элемент: '))
     return array
 
 def replace_pair(array):
     """ Replace pair elements of list"""
     i = 0
-    if len(array) % 2 == 0:
+    result = array.copy()
+    if len(result) % 2 == 0:
         while i < len(array):
             if i % 2 == 0 or i == 0:
-                temp = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = temp
+                temp = result[i]
+                result[i] = result[i + 1]
+                result[i + 1] = temp
             i += 1
-    elif len(array) % 2 != 0:
-        while i < len(array)-1:
+    elif len(result) % 2 != 0:
+        while i < len(result)-1:
             if i % 2 == 0 or i == 0:
-                temp = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = temp
+                temp = result[i]
+                result[i] = result[i + 1]
+                result[i + 1] = temp
             i += 1
+    return result
+
+def replace_pair_another(array):
+    """ Replace pair elements of list"""
+    size_of_array = len(array)
+    if size_of_array % 2 == 0:
+        for i in range(0, size_of_array):
+            if i % 2 == 0 or i == 0:
+                array[i], array[i + 1] = array[i + 1], array[i]
+    elif size_of_array % 2 != 0:
+        for i in range(0, size_of_array - 1):
+            if i % 2 == 0 or i == 0:
+                array[i], array[i + 1] = array[i + 1], array[i]
     return array
 
 size = int(input("Введите количество элементов списка: "))
@@ -36,3 +49,5 @@ my_list = create_list(size)
 print(my_list)
 result_list = replace_pair(my_list)
 print(result_list)
+result_list_another = replace_pair_another(my_list)
+print(result_list_another)
