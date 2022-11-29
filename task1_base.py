cb81ef44033d768376cefdd66cf46b556982855c
Ivan Ustyantsev <vangold915@gmail.com>
2022-11-29 17:50:32 +0700

    Created replace_pair function

--- task1_base.py
+++ task1_base.py
@@ -6,19 +6,19 @@
 """
 # my_list = [1, "Привет", True, [1, 2, 3], None]
 # my_list = list()
-# my_list = []
-# my_list.append(1)
-# my_list.append("Привет")
-# my_list.append(True)
-# my_list.append([1, 2, 3])
-# my_list.append(None)
-#
-#
-# def check_type(array):
-#     """checking the type of each element"""
-#     i = 0
-#     for el_arr in array:
-#         print(f'{i} элемент имеет тип: {type(el_arr)}"')
-#         i += 1
-#
-# check_type(my_list)
+my_list = []
+my_list.append(1)
+my_list.append("Привет")
+my_list.append(True)
+my_list.append([1, 2, 3])
+my_list.append(None)
+
+
+def check_type(array):
+    """checking the type of each element"""
+    i = 0
+    for el_arr in array:
+        print(f'{i} элемент имеет тип: {type(el_arr)}"')
+        i += 1
+
+check_type(my_list)
--- task2_base.py
+++ task2_base.py
@@ -12,7 +12,27 @@ def create_list(array_size):
         array.append(input(f'Введите {i} строчный элемент: '))
     return array
 
+def replace_pair(array):
+    """ Replace pair elements of list"""
+    i = 0
+    if len(array) % 2 == 0:
+        while i < len(array):
+            if i % 2 == 0 or i == 0:
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+            i += 1
+    elif len(array) % 2 != 0:
+        while i < len(array)-1:
+            if i % 2 == 0 or i == 0:
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+            i += 1
+    return array
 
 size = int(input("Введите количество элементов списка: "))
 my_list = create_list(size)
 print(my_list)
+result_list = replace_pair(my_list)
+print(result_list)
